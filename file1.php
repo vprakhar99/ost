d4ea9cf64d5fa817eac6470bb7bbdc31718e85b3
vprakhar99 <vprakhar99@gmail.com>
2020-09-10 10:10:31 +0530

    .

---
+++ file1.php
@@ -0,0 +1,12 @@
+<?php
+$x="Blue";
+switch($x){
+case "Red": echo "Colour is Red";
+break;
+case "Blue": echo "Colour is Blue";
+break;
+case "Yellow": echo "Colour is Yellow";
+break;
+default: echo "It isn't red, blue or green";
+}
+?>
